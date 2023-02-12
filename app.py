8c551f7905caa5a4dc4e3c811702930e1ea2c8a8
maverickk4 <124333912+maverickk4@users.noreply.github.com>
2023-02-11 22:35:47 -0500

    up and going

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 import requests
 
 app = Flask(__name__)
-app.config['SESSION_COOKIE_SAMESITE'] = 'Strict'
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
 api_key = "553ad8b749104d99b6aa7de7a2928614"
 
 @app.route('/')
@@ -12,10 +12,11 @@ def index():
 @app.route('/search', methods=['POST'])
 def search():
     search_term = request.form.get('search_term')
-    url = f"https://newsapi.org/v2/everything?q={search_term}&pageSize=5&apiKey={api_key}"
+    url = f"https://newsapi.org/v2/everything?q={search_term}&pageSize=10&sortBy=relevancy&apiKey={api_key}"
+
     response = requests.get(url)
     articles = response.json().get('articles', [])
     return render_template('index.html', articles=articles)
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host='http://ec2-3-210-186-25.compute-1.amazonaws.com/', port=80)
